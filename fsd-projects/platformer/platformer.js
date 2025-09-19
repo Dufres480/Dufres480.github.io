a15a7f44d0dd2caee49fab41e2f4983c8bac62f4
Dufres480 <Dufres480@wearescpps.org>
2025-09-19 17:58:53 +0000

    Perfected version of platform

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,31 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
-
+     //toggleGrid();
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(300,700,50,30);
+    createPlatform(550,600,50,30,);
+    createPlatform(300,500,50,30);
+    createPlatform(550,400,50,30,"yellow");
+    createPlatform(850,350,50,30,"yellow");
+    createPlatform(1150,300,50,30,"yellow");
+    createPlatform(1250,200,50,30,"yellow");
+    createPlatform(1300,100,100,25,"yellow");
+    createPlatform(0,400,50,30,"red");
+    createPlatform(250,300,45,30,"red");
+    createPlatform(550,200,75,20,"red");
+    createPlatform(700,0,10,180,"red");
+    createPlatform(250,200,44.9,10,"red");
+    
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("diamond",1300,50,0,0);
+    createCollectable("steve",600,150,0,0);
+    createCollectable("grace");
     // TODO 4 - Create Cannons
-
-
-    
-    
+  //createCannon("right",700,0);
+  createCannon("right",800,0);
+  createCannon("top",600,1000);
+  createCannon("right",500,1200);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
